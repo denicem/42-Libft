f55cc08c8601311bc73f27b738d6dbffaad5ffcf
Denice M <denicem@hotmail.de>
2021-08-18 16:35:03 +0200

    add ft_strrchr.c

--- libft.h
+++ libft.h
@@ -31,7 +31,7 @@ size_t ft_strlen(const char *c);
 int ft_toupper(int c);
 int ft_tolower(int c);
 char *ft_strchr(const char *s, int c);
-//char *ft_strrchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
 int ft_strncmp(const char *s1, const char *s2);
 //void *ft_memchr(const void *s, int c, size_t n);
 //int ft_memcmp(const void *s1, const void *s2);
