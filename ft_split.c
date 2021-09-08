975edf7b73a7532f559e41bc609e36467eea0660
Denice Montemayor <dmontema@2-E-5.42heilbronn.de>
2021-09-08 18:57:19 +0200

    modify ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,22 +6,49 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 15:16:10 by dmontema          #+#    #+#             */
-/*   Updated: 2021/09/07 18:41:05 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/09/08 18:44:17 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_allocStr(int size)
+static void	ft_freeMem(char **res, int pos)
 {
-	char *res;
+	while (pos >= 0)
+	{
+		free(res[pos]);
+		pos--;
+	}
+	free(res);
+}
 
-	res = malloc(sizeof(char) * size + 1);
-	if (res == NULL)
-		return (NULL);
-	ft_bzero(res, size + 1);
+static int	ft_insertStr(char const *s, char c, char **res)
+{
+	int	str_len;
+	int	i;
 
-	return (res);
+	i = 0;
+	while (*s != '\0')
+	{
+		str_len = 0;
+		while (*s != c && *s != '\0')
+		{
+			str_len++;
+			s++;
+		}
+		res[i] = (char *) malloc(sizeof(char) * str_len + 1);
+		if (res[i] == NULL)
+		{
+			ft_freeMem(res, i);
+			return (0);
+		}
+		ft_strlcpy(res[i], s - str_len, str_len + 1);
+		i++;
+		while (*s == c && *s != '\0')
+			s++;
+	}
+	res[i] = 0;
+	return (1);
 }
 
 static int	ft_getArrSize(char const *s, char c)
@@ -44,31 +71,15 @@ static int	ft_getArrSize(char const *s, char c)
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
-	int		str_len;
-	int		i;
-	
-	if (s == NULL || c == '\0')
+
+	if (c == '\0' || s == NULL)
 		return (NULL);
 	while (*s == c)
 		s++;
-	res = malloc(sizeof(char *) * ft_getArrSize(s, c) + 1);
+	res = (char **) malloc(sizeof(char *) * ft_getArrSize(s, c) + 1);
 	if (res == NULL)
 		return (NULL);
-	i = 0;
-	while (*s != '\0')
-	{
-		str_len = 0;
-		while (*s != c && *s != '\0')
-		{
-			str_len++;
-			s++;
-		}
-		*(res + i) = ft_allocStr(str_len);
-		ft_strlcpy(*(res + i), s - str_len, str_len + 1);
-		i++;
-		while (*s == c)
-			s++;
-	}
-	*(res + i) = 0;
+	if (!ft_insertStr(s, c, res))
+		return (NULL);
 	return (res);
 }
